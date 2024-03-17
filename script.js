7f50eac248ee48bfe4d9b330c6c729f3667c0ed4
ANKUSHUKIL03 <142995201+ANKUSHUKIL03@users.noreply.github.com>
2024-03-18 00:14:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("buyer").addEventListener("click", function() {
+    // Redirect to login pindex.htmlage
+    window.location.href = "./pages/buyer.html";
+});
+document.getElementById("seller").addEventListener("click", function() {
+    // Redirect to login page
+    window.location.href = "./pages/seller.html";
+});
